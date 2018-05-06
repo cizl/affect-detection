e8c8ab0bd9b473a0ec63ef66702033a7c9d81bae
cizl <david.lozic@fer.hr>
2018-05-06 23:07:07 +0200

    add one-hot categorical labels to ordinal classification

--- data.py
+++ data.py
@@ -6,6 +6,7 @@ import bpdb
 
 import numpy as np
 from keras.preprocessing.text import Tokenizer
+from keras.utils.np_utils import to_categorical
 from tqdm import tqdm, trange
 #from keras.preprocessing.text import pad_sequences
 
@@ -24,17 +25,20 @@ def get_data(data_file):
       label = fields[-1]
 
       if 'test' not in data_file:
-        if '-reg-' in data_file:
+        if '-reg-' in data_file: # float, e.g. 0.73
           label = float(label)
-        elif '-oc-' in data_file:
+        elif '-oc-' in data_file: # int to one hot, e.g. 3 -> [0, 0, 0, 1]
           label = int(label.split(':')[0])
-        elif '-c-' in data_file:
+        elif '-c-' in data_file: # one hot multilabel, e.g. [1, 0, 1, 1, 0]
           label = [int(x) for x in fields[2:]]
 
       tweets.append(tweet)
       labels.append(label)
 
-  return tweets, labels
+  if '-oc' in data_file:
+    labels = to_categorical(labels)
+
+  return np.array(tweets), np.array(labels)
 
 
 def prepare_word_index(data_dir):
@@ -85,15 +89,22 @@ def prepare_embedding_index(embedding_file):
 
 def prepare_embedding_matrix(word_index, embedding_index):
   print('Preparing embedding matrix..')
+  total = 0
+  hits = 0
   emb_dim = len(list(embedding_index.values())[0])
   embedding_matrix = np.zeros((len(word_index) + 1, emb_dim))
   for word, index in tqdm(word_index.items()):
     embedding = embedding_index.get(word)
     if embedding is not None:
       embedding_matrix[index] = embedding
+      hits += 1
+    total += 1
 
   with open('embedding_matrix.pickle', 'wb') as fout:
     pickle.dump(embedding_matrix, fout)
+
+  print('Matched', hits, 'out of ', total,
+        'words from the corpus to their embedding vectors')
 
   return embedding_matrix
 
@@ -107,11 +118,6 @@ if __name__ == '__main__':
 
   args = vars(parser.parse_args())
 
-  
-  x, y= get_data(args['data_dir'])
-  print(x[-1], y[-1])
-
-  sys.exit(1)
   word_index = prepare_word_index(args['data_dir'])
   emb_index = prepare_embedding_index(args['embedding_file'])
   emb_matrix = prepare_embedding_matrix(word_index, emb_index)
