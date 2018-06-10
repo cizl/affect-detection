2a9dbde56ec4d7b2c7d20c2ef211bce1801ae3f0
cizl <david.lozic@fer.hr>
2018-06-10 23:48:50 +0200

    fix grid_search bug

--- grid_search.py
+++ grid_search.py
@@ -21,64 +21,78 @@ from tqdm import tqdm
 
 from metrics import PearsonCallback
 from data import prepare_word_index, get_data
+from data import prepare_embedding_index, prepare_embedding_matrix
+from data import prepare_affect_index, prepare_affect_matrix
 from models import create_embedding_layer
 from models import architectures, param_grid
 
 
-def model_parametrizer(architecture, param_grid, embedding_matrix):
+def build_model(architecture, params, embedding_matrix, verbose=False):
+  model = Sequential()
+  model.add(create_embedding_layer(embedding_matrix))
+  for layer, arg in zip(architecture, params):
+    model.add(layer(*arg))
+ 
+  model.add(Flatten())
+  if verbose:
+    print(model.summary())
+  
+  return model
+
+def parametrizer(architecture, param_grid):
   args = []
   for layer in architecture:
     args.append(list(itertools.product(*param_grid[layer])))
 
   all_args = list(itertools.product(*args))
   for args in tqdm(all_args):
-    model = Sequential()
-    model.add(create_embedding_layer(embedding_matrix))
-    for layer, arg in zip(architecture, args):
-      model.add(layer(*arg))
+    yield args
 
-    model.add(Flatten())
-
-#    print(model.summary())
-    yield model
-
-def grid_search(X, y, val_x, val_y, architecture_grid, parameter_grid, n_folds, corpus, embedding_matrix, model_dir):
+def grid_search(X, y, val_x, val_y, architecture_grid, param_grid, n_folds, corpus, embedding_matrix, model_dir):
   best_score = 0 
   scores = {}
+  models = {}
 
   for architecture in architecture_grid:
-    model_gen = model_parametrizer(architecture, parameter_grid, embedding_matrix)
-    for model in model_gen:
-      if '-reg-' in corpus:
-        model.add(Dense(1, activation='sigmoid'))
-      if '-oc-' in corpus:
-        model.add(Dense(1, activation='linear'))
-      if '-c-' in corpus:
-        model.add(Dense(11, activation='sigmoid'))
+    param_gen = parametrizer(architecture, param_grid)
 
-      if '-reg-' in corpus:
-        loss = 'mean_squared_error'
-      else:
-        loss = 'categorical_crossentropy'
-
-      early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
-      checkpoint = ModelCheckpoint(filepath=os.path.join(model_dir, 'weights.hdf5'), verbose=0, save_best_only=True)
-      callbacks = [early_stopping, checkpoint]
-
-      metrics = []
-      if '-oc-' in corpus:
-        metrics.append('acc')
-
-      model.compile(loss=loss, optimizer='adam', metrics=metrics)
-
+    for params in param_gen:
+      print('params:', params)
       avg_score = 0
-      for i in range(n_folds):
-        val_start = i * len(X) // n_folds
-        val_end = (i+1) * len(X) // n_folds
+      for l in range(n_folds):
+#      for k in range(1):
+        val_start = l * len(X) // n_folds
+        val_end = (l+1) * len(X) // n_folds
         x_val = X[val_start:val_end]
         y_val = y[val_start:val_end]
         x_train = np.vstack([X[:val_start], X[val_end:]])
         y_train = np.concatenate([y[:val_start], y[val_end:]])
+
+        model = build_model(architecture, params, embedding_matrix)
+#        print(model.summary())
+        if '-reg-' in corpus:
+          model.add(Dense(1, activation='linear'))
+        if '-oc-' in corpus:
+          model.add(Dense(1, activation='linear'))
+        if '-c-' in corpus:
+          model.add(Dense(11, activation='sigmoid'))
+
+        if '-reg-' in corpus:
+          loss = 'mean_squared_error'
+        elif '-oc' in corpus:
+          loss = 'sparse_categorical_crossentropy'
+        elif '-c-' in corpus:
+          loss = 'categorical_crossentropy'
+
+        early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='auto')
+        checkpoint = ModelCheckpoint(filepath=os.path.join(model_dir, 'weights.hdf5'), verbose=0, save_best_only=True)
+        callbacks = [early_stopping, checkpoint]#, PearsonCallback()]
+
+        metrics = []
+        if '-oc-' in corpus:
+          metrics.append('acc')
+
+        model.compile(loss=loss, optimizer='adam', metrics=metrics)
 
         model.fit(x_train, y_train, 
                   validation_data=(x_val, y_val),
@@ -90,45 +104,38 @@ def grid_search(X, y, val_x, val_y, architecture_grid, parameter_grid, n_folds, 
         n_params = int(np.sum([K.count_params(p) for p in set(model.trainable_weights)]))
         y_pred = model.predict(val_x).reshape(1, -1)[0]
         score = pearsonr(val_y, y_pred)[0]
+        print('score:', score)
         avg_score += score
 
       avg_score /= n_folds
-      print('avg:', avg_score)
-
-      if avg_score > best_score:
-        # finally, train on all data
-        model.fit(X, y, 
-                  validation_data=(val_x, val_y),
-                  epochs=300, 
-                  batch_size=64,
-                  callbacks=callbacks,
-                  verbose=0)
-        model.save(os.path.join(model_dir, 'best.hdf5'))
-        y_pred = model.predict(val_x).reshape(1, -1)[0]
-        score = pearsonr(val_y, y_pred)[0]
-        print(n_params)
-        print(score)
+      print('avg:', avg_score, params)
 
       try:
-        if score > scores[n_params]:
-          scores[n_params] = score
+        if avg_score > scores[n_params]:
+          scores[n_params] = avg_score
+          models[n_params] = (architecture, params)
       except KeyError:
-        scores[n_params] = score
+        scores[n_params] = avg_score
+        models[n_params] = (architecture, params)
+      
+      with open('history.pickle', 'wb') as fout:
+        pickle.dump((scores, models), fout)
 
-  return scores
+  return scores, models
 
 
-def train(corpus_file, model_dir, embeddings, fresh_run=False, data_dir=None,
+def train(corpus_file, model_dir, embeddings, affect_lexicon, fresh_run=False, data_dir=None,
           epochs=3, batch_size=64, val_split=0.2):
 
   if not os.path.exists(model_dir):
     os.makedirs(model_dir)
-#    fresh_run=True
 
   if fresh_run:
     word_index = prepare_word_index(data_dir)
     embedding_index = prepare_embedding_index(embeddings)
     embedding_matrix = prepare_embedding_matrix(word_index, embedding_index)
+    affect_index = prepare_affect_index(affect_lexicon)
+    affect_matrix = prepare_affect_matrix(word_index, affect_index)
   else:
     with open('word_index.pickle', 'rb') as fin:
       word_index = pickle.load(fin)
@@ -136,8 +143,14 @@ def train(corpus_file, model_dir, embeddings, fresh_run=False, data_dir=None,
       embedding_index = pickle.load(fin)
     with open('embedding_matrix.pickle', 'rb') as fin:
       embedding_matrix = pickle.load(fin)
+    with open('affect_index.pickle', 'rb') as fin:
+      affect_index = pickle.load(fin)
+    with open('affect_matrix.pickle', 'rb') as fin:
+      affect_matrix = pickle.load(fin)
 
   tweets, labels = get_data(corpus_file)
+  print(tweets[:5])
+  print(labels[:5])
 
   tokenizer = Tokenizer()
   tokenizer.word_index = word_index
@@ -147,10 +160,10 @@ def train(corpus_file, model_dir, embeddings, fresh_run=False, data_dir=None,
   x_train, x_val, y_train, y_val = train_test_split(
       sequences, labels, test_size=val_split, random_state=42)
 
-  history = grid_search(x_train, y_train, x_val, y_val, architectures, param_grid, 5, '-reg-', embedding_matrix, model_dir)
+  scores, models = grid_search(x_train, y_train, x_val, y_val, architectures, param_grid, 5, corpus_file, embedding_matrix, model_dir)
  
   with open('history.pickle', 'wb') as fout:
-    pickle.dump(history, fout)
+    pickle.dump((scores, models), fout)
 
 
 if __name__ == '__main__':
@@ -161,6 +174,8 @@ if __name__ == '__main__':
                       help='Model data dir. (weights, tokens, etc.)')
   parser.add_argument('-em', '--embeddings', required=True,
                       help='Embeddings file.')
+  parser.add_argument('-a', '--affect_lexicon', required=True,
+                      help='Path to affect lexicon (txt).')
   parser.add_argument('-e', '--epochs', type=int, default=3,
                       help='Number of training epochs.')
   parser.add_argument('-b', '--batch_size', type=int, default=64,
