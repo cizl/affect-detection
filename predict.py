eacdbb9808fe13cf88008c7c47a6382d954a1991
cizl <david.lozic@fer.hr>
2018-06-10 23:48:28 +0200

    swap predict and predict_all, they were wrongly named lol

--- predict.py
+++ predict.py
@@ -1,7 +1,7 @@
 import os
-import re
 import argparse
 import pickle
+import re
 
 import numpy as np
 import keras
@@ -18,27 +18,16 @@ from models import get_model
 from metrics import PearsonCallback
 
 
-def predict_dir(data_dir, model_dir, embeddings):
-
-  for corpus_file in os.listdir(data_dir):
-    if 'dev' not in corpus_file:
-      continue
-    print('Predicting:', corpus_file)
-
-    corpus_path = os.path.join(data_dir, corpus_file)
-    preds = predict(corpus_path, model_dir, embeddings)
-    preds_file = corpus_file.split('.')[0] + '.npy'
-    np.save(os.path.join(model_dir, preds_file), preds)
-
-
-def predict(corpus_path, model_dir, embeddings):
+def predict(corpus_file, model_dir, embeddings):
 
   with open('word_index.pickle', 'rb') as fin:
     word_index = pickle.load(fin)
+  with open('embedding_index.pickle', 'rb') as fin:
+    embedding_index = pickle.load(fin)
   with open('embedding_matrix.pickle', 'rb') as fin:
     embedding_matrix = pickle.load(fin)
 
-  tweets, labels = get_data(corpus_path)
+  keys, tweets, labels = get_data(corpus_file, return_keys=True)
 
   tokenizer = Tokenizer()
   tokenizer.word_index = word_index
@@ -46,31 +35,30 @@ def predict(corpus_path, model_dir, embeddings):
   sequences = pad_sequences(sequences, maxlen=50)
 
   adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=10e-9, decay=0.0, amsgrad=False)
-  model = get_model(model_dir, corpus_path, embedding_matrix)
-  corpus_file = os.path.basename(corpus_path)
-
-  weights_paths = os.listdir(model_dir)
-  search = re.sub('2018-', '', corpus_file)
-  search = re.sub('.txt', '', search)
-  search = re.sub('-dev', '', search)
-  #print(search)
-  weights_file = [match for match in weights_paths if search in match][0]
-  weights_path = os.path.join(model_dir, weights_file)
-  #weights_path = os.path.join(model_dir, corpus_file.split('.')[0] + '.hdf5')
-  model.load_weights(weights_path)
+  model = get_model(model_dir, corpus_file, embedding_matrix)
+  model.load_weights(os.path.join(model_dir, 'weights.hdf5'))
   model.compile(loss='mean_squared_error', optimizer=adam)
 
   predictions = model.predict(sequences)
-  #print(pearsonr(labels.reshape(-1, 1), predictions))
+
+  os.makedirs('submissions', exist_ok=True)
+  submission = os.path.basename(corpus_file)
+  submission = re.sub('2018-', '', submission)
+  submission = re.sub('-En-', '_en_', submission)
+  submission = re.sub('-test', '_pred', submission)
+  with open(os.path.join('submissions', submission), 'w') as fout:
+    print('ID\tTweet\tAffect\tDimension\tIntensity\tScore', file=fout)
+    for k, t, l, p in zip(keys, tweets, labels, predictions):
+      print('\t'.join([k, t, l, str(p[0])]), file=fout)
+    
+  print(pearsonr(labels.reshape(-1, 1), predictions))
   return predictions
 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Train a deep NLP model')
-  parser.add_argument('-c', '--corpus_path',
+  parser.add_argument('-c', '--corpus_file', required=True,
                       help='Training corpus - txt file.')
-  parser.add_argument('-d', '--data_dir',
-                      help='Data directory. (for generating all predictions)')
   parser.add_argument('-m', '--model_dir', required=True,
                       help='Model data dir. (weights, tokens, etc.)')
   parser.add_argument('-em', '--embeddings', required=True,
@@ -84,9 +72,4 @@ if __name__ == '__main__':
   if gpu is not None:
     os.environ["CUDA_VISIBLE_DEVICES"] = gpu
 
-  if args['corpus_path'] is not None: # predict file
-    args.pop('data_dir')
-    predict(**args)
-  elif args['data_dir'] is not None: # predict directory
-    args.pop('corpus_path')
-    predict_dir(**args)
+  predict(**args)
